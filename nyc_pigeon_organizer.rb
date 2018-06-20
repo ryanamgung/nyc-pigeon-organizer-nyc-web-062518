8262ad88bca4c2e8e2f115da8f2b48366bd5573d
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 05:13:34 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,39 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  new_hash = {}
-  data[:gender].each do |gen, names|
-    new_hash[names] = {}
-  end
-  
-  data[:color].each do |hash|
-    hash.each do |color, names|
-      new_hash.each do |name, hash|
-        if names == name
-          new_hash[name][:color] = [color]
-        end
-      end
-    end
-  end
-  
-  data[:gender].each do |hash|
-    hash.each do |gender, names|
-      new_hash.each do |name, hash|
-        if names == name
-          new_hash[name][:gender] = [gender]
-        end
-      end
-    end
-  end
-  
-  data[:lives].each do |hash|
-    hash.each do |location, names|
-      new_hash.each do |name, hash|
-        if names == name
-          new_hash[name][:lives] = [location]
-        end
-      end
-    end
-  end
-  
-  new_hash
+
++  organized_hash = {}
++  seven_pigeons.each do |name|
++    organized_hash[name] = {}
++  end
++  data.each do |attribute, details_hash|
++    organized_hash.each do |quality, data|
++      organized_hash[quality][attribute] = []
++    end
++  end
++  data.each do |attribute, details_hash|
++    organized_hash.each do |quality, data|
++      details_hash.each do |key, value|
++        if value.include? quality
++          organized_hash[quality][attribute] << key.to_s
++        end
++      end
++    end
++  end
++  organized_hash
+
+all_names = data.map do |types, hash|
+  hash.values
+end
+all_names = all_names.flatten.uniq
+
+
+
 end
