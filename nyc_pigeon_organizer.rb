085228cc569fb1f623b87a8e513159b94ae94857
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 05:47:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  all_names = data.map do |types, hash|
+    hash.values
+  end
+  all_names = all_names.flatten.uniq
+  
+  all_names.each do |name|
+    new_hash[name] = {}
+  end
+  
+  data.each do |type, hash|
+    new_hash.each do |name, info|
+      new_hash[name][type] = []
+    end
+  end
+  
+  data.each do |type, hash|
+    new_hash.each do |names, kind|
+      hash.each do |attribute, name|
+        name.each do |the_name|
+          if the_name == names
+            new_hash[names][type] << attribute.to_s
+          end
+        end
+      end
+    end
+  end
+  
+  new_hash
 end
