90ffd8e1eb3ed0500d29b651a7e0007086df91b6
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 05:20:20 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,5 @@
 def nyc_pigeon_organizer(data)
 
-+  organized_hash = {}
 +  seven_pigeons.each do |name|
 +    organized_hash[name] = {}
 +  end
@@ -19,11 +18,16 @@ def nyc_pigeon_organizer(data)
 +    end
 +  end
 +  organized_hash
+new_hash = {}
 
 all_names = data.map do |types, hash|
   hash.values
 end
 all_names = all_names.flatten.uniq
+
+all_names.each do |name|
+
+end
 
 
 
