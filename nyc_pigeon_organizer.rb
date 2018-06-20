e3d51d86e73bbef4aae600c6eecfe709a17ca912
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 05:34:38 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,22 +9,22 @@ def nyc_pigeon_organizer(data)
 +      end
 +    end
 +  end
-new_hash = {}
-
-all_names = data.map do |types, hash|
-  hash.values
-end
-all_names = all_names.flatten.uniq
-
-all_names.each do |name|
-  new_hash[name] = {}
-end
-
-data.each do |type, hash|
-  new_hash.each do |name, info|
-    new_hash[name][type] = []
+  new_hash = {}
+  
+  all_names = data.map do |types, hash|
+    hash.values
   end
-end
+  all_names = all_names.flatten.uniq
+  
+  all_names.each do |name|
+    new_hash[name] = {}
+  end
+  
+  data.each do |type, hash|
+    new_hash.each do |name, info|
+      new_hash[name][type] = []
+    end
+  end
 
 
 
