95d0b77898d1f93aa844dc61f1a4d6218f959eeb
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 05:01:00 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,3 @@
-+data.each_with_object({}) do |(key, hash), pigeon_list|
--end 	+  hash.each do |attribute, pigeon_array|
-+    pigeon_array.each do |pigeon|
-+      pigeon_list[pigeon] ||= {}
-+      pigeon_list[pigeon][key] ||= []
-+      pigeon_list[pigeon][key] << attribute.to_s
-+      #binding.pry
-+    end
-+
-+  end
-+
-+end
-
 def nyc_pigeon_organizer(data)
   # write your code here!
   data[:gender].each do |color|
