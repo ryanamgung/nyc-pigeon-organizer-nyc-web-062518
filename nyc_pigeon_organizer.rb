be34393df40a8ba298c4cf9920e4a46aaea9d80c
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 05:09:08 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   data[:gender].each do |gen, names|
-    new_hash[names] => {}
+    new_hash[names] = {}
   end
   
   data[:color].each do |hash|
